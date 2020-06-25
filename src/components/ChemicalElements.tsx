21b1eca94ff71b64c77ff6906aac8bd0539e9e2e
raseshsingh <raseshyunique@gmail.com>
2020-06-26 03:44:19 +0530

    Object Not Rendered

--- src/components/ChemicalElements.tsx
+++ src/components/ChemicalElements.tsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './ChemicalElements.css';
 import {IonRow, IonCol, IonIcon, IonSlide, IonSlides} from "@ionic/react";
-import {checkDatabase} from "../firebaseConfig";
+import {getMetals} from "../firebaseConfig";
 import {closeCircle, gameController, gift, heart, homeOutline} from "ionicons/icons";
 
 import '../theme/variables.css';
@@ -10,16 +10,30 @@ import Slide from "./Slide";
 
 const slideOpts = {slidesPerView: 1.2}
 
+async function waitF() {
+
+    const preObject = document.getElementById('object');
+    // @ts-ignore
+    preObject.innerText = JSON.stringify(getMetals(), null, 3)
+    console.log(getMetals())
+    return await getMetals()
+}
 // @ts-ignore
 class ChemicalElements extends Component {
     constructor() {
         // @ts-ignore
         super();
+        this.state = {
+            metals: {}
+        }
         console.log('constructor called')
     }
 
     componentDidMount() {
-        checkDatabase()
+        this.setState({
+            metals: waitF()
+        })
+// Get Element
     }
 
 
@@ -42,7 +56,10 @@ class ChemicalElements extends Component {
                     </IonCol>
                     <IonCol size={'10'}>
                         <IonSlides className={'overflow-visible'} options={slideOpts}>
-                            <Slide/>
+
+
+                            <Slide name={'Amricium'} sign={'Rm'} type={'Radioactive'}
+                                   img={'https://i.ibb.co/drTsWdN/Any-Conv-com-ele-11.jpg'}/>
                             <Slide/>
                             <Slide/>
                             <Slide/>
